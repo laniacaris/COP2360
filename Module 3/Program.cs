c8ff65dd00548b04fa66bdaf1ce6bdc948884cca
laniacaris <laniacaris@gmail.com>
2025-09-19 19:13:24 -0400

    Update Program.cs

--- Module 3/Program.cs
+++ Module 3/Program.cs
@@ -1,1 +1,27 @@
+using System;
 
+class Octopus
+{
+    string name;
+    public int Age = 24;
+    static readonly int legs = 8;
+    static readonly int eyes = 1;
+    public readonly string Name;
+    public readonly int Legs = 8;
+
+    public Octopus(string name)
+    {
+        Name = name;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var o = new Octopus("Larry");
+        Console.WriteLine("Age: " + o.Age);
+        Console.WriteLine("Name: " + o.Name);
+        Console.ReadLine();
+    }
+}
